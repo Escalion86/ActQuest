d206ab7926814173434154994b709dddbd484db3
Escalion <escalion86@gmail.com>
2023-05-10 18:44:53 +0700

    test2

--- telegram/createTeam.js
+++ telegram/createTeam.js
@@ -2,6 +2,27 @@ import LastCommands from '@models/LastCommands'
 import dbConnect from '@utils/dbConnect'
 import sendMessage from './sendMessage'
 
+const script = async ({ userTelegramId, command, text, keyboard }) => {
+  if (command)
+    await LastCommands.findOneAndUpdate(
+      {
+        userTelegramId,
+      },
+      { command },
+      { upsert: true }
+    )
+  else
+    await LastCommands.findOneAndDelete({
+      userTelegramId,
+    })
+  return await sendMessage({
+    chat_id: userTelegramId,
+    // text: JSON.stringify({ body, headers: req.headers.origin }),
+    text,
+    keyboard,
+  })
+}
+
 var keyboard = {
   inline_keyboard: [
     [
@@ -19,17 +40,23 @@ const createTeam = async (userTelegramId, command, res) => {
   console.log('userTelegramId :>> ', userTelegramId)
   console.log('lastCommand :>> ', lastCommand)
   if (lastCommand.length === 0) {
-    console.log('==> /create_team/set_name')
-    await LastCommands.create({
+    return await async({
       userTelegramId,
       command: '/create_team/set_name',
-    })
-    return await sendMessage({
-      chat_id: userTelegramId,
-      // text: JSON.stringify({ body, headers: req.headers.origin }),
       text: 'Введите название команды',
       keyboard,
     })
+    // console.log('==> /create_team/set_name')
+    // await LastCommands.create({
+    //   userTelegramId,
+    //   command: '/create_team/set_name',
+    // })
+    // return await sendMessage({
+    //   chat_id: userTelegramId,
+    //   // text: JSON.stringify({ body, headers: req.headers.origin }),
+    //   text: 'Введите название команды',
+    //   keyboard,
+    // })
     // return res?.status(200).json({ success: true, data })
   } else {
     // Если небыло никаких команд с пользователем
@@ -44,14 +71,18 @@ const createTeam = async (userTelegramId, command, res) => {
       })
     }
 
-    if (commandsArray[1] === 'set_name') {
-      console.log('error create_team')
-      return await sendMessage({
-        chat_id: userTelegramId,
-        // text: JSON.stringify({ body, headers: req.headers.origin }),
+    if (commandsArray[1] === 'set_name')
+      return await script({
+        userTelegramId,
+        command: '/create_team/set_description',
         text: `Задано название команды: ${command}.\nВведите описание команды (не обязательно)`,
+        keyboard,
       })
-    }
+    if (commandsArray[1] === 'set_description')
+      return await script({
+        userTelegramId,
+        text: `Задано описание команды: ${command}. Создание команды завершено`,
+      })
 
     // return res?.status(200).json({ success: true, data: command })
   }
