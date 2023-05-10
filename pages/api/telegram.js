1247ca734e0b64b86f213bb0263424baba00d2d8
Escalion <escalion86@gmail.com>
2023-05-11 03:13:50 +0700

    test2

--- pages/api/telegram.js
+++ pages/api/telegram.js
@@ -156,8 +156,8 @@ export default async function handler(req, res) {
       if (body?.callback_query) {
         // Принимаем команду
         // console.log('callback_body :>> ', body)
-        const res = callbackHandler(body, res)
-        console.log('res :>> ', res)
+        const result = callbackHandler(body, res)
+        console.log('result :>> ', result)
         // await sendMessage({
         //   chat_id: '261102161',
         //   // text: JSON.stringify({ body, headers: req.headers.origin }),
@@ -169,8 +169,8 @@ export default async function handler(req, res) {
         // console.log('message_body :>> ', body)
         // const { message_id, from, chat, date, text, entities } = message
         // const {id, from, message, chat_instanse, data}
-        const res = messageHandler(body, res)
-        console.log('res :>> ', res)
+        const result = messageHandler(body, res)
+        console.log('result :>> ', result)
         // await sendMessage({
         //   chat_id: '261102161',
         //   // text: JSON.stringify({ body, headers: req.headers.origin }),
