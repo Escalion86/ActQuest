90f90b82ae954f0275504302aca21007ce0c244f
Escalion <escalion86@gmail.com>
2023-05-21 22:54:38 +0700

    test8 join_team

--- telegram/func/getTeam.js
+++ telegram/func/getTeam.js
@@ -1,9 +1,13 @@
 import Teams from '@models/Teams'
 import dbConnect from '@utils/dbConnect'
+import mongoose from 'mongoose'
 
 const getTeam = async (id) => {
   await dbConnect()
-  return await Teams.findById(id)
+  const preparedId = mongoose.Types.ObjectId(id)
+  return await Teams.findById(preparedId, null, (err) => {
+    console.log('err :>> ', err)
+  })
 }
 
 export default getTeam
