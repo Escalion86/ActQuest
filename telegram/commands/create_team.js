ea289c981a0c4f4189bd115eed613d43b29a15de
Escalion <escalion86@gmail.com>
2023-05-21 15:30:49 +0700

    test7

--- telegram/commands/create_team.js
+++ telegram/commands/create_team.js
@@ -4,24 +4,25 @@ import createTeam from 'telegram/func/createTeam'
 const create_team = async ({ telegramId, message, props }) => {
   await dbConnect()
   // Если задаем имя
-  if (!props.teamName) {
-    // if (!message)
-    return {
-      success: true,
-      message: 'Введите название команды',
-      // nextCommand: `/menu_teams`,
-    }
+  if (!message) {
+    if (!props.teamName)
+      return {
+        success: true,
+        message: 'Введите название команды',
+        // nextCommand: `/menu_teams`,
+      }
+
+    // Если имя уже задано, значит сейчас идет ввод описания
+    const team = await createTeam(telegramId, props?.teamName, message)
+
+    return { success: true, message: `Команда "${props?.teamName}" создана` }
   }
+
   return {
     success: true,
     message: `Задано название команды "${message}"`,
     nextCommand: `/create_team/teamName=${message}`,
   }
-
-  // Если имя уже задано, значит сейчас идет ввод описания
-  const team = await createTeam(telegramId, props?.teamName, message)
-
-  return { success: true, message: `Команда "${props?.teamName}" создана` }
 }
 
 export default create_team
