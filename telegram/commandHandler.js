9fc157eaa8d661961c54092b03c8f6c7787ced86
Escalion <escalion86@gmail.com>
2023-05-21 15:44:13 +0700

    test7

--- telegram/commandHandler.js
+++ telegram/commandHandler.js
@@ -8,186 +8,6 @@ import inlineKeyboard from './func/inlineKeyboard'
 import keyboardFormer from './func/keyboardFormer'
 import sendMessage from './sendMessage'
 
-const updateCommand = async (userTelegramId, command) => {
-  if (command)
-    await LastCommands.findOneAndUpdate(
-      {
-        userTelegramId,
-      },
-      { command },
-      { upsert: true }
-    )
-  else
-    await LastCommands.findOneAndDelete({
-      userTelegramId,
-    })
-}
-
-const script = async ({ userTelegramId, command, text, keyboard }) => {
-  if (command)
-    await LastCommands.findOneAndUpdate(
-      {
-        userTelegramId,
-      },
-      { command },
-      { upsert: true }
-    )
-  else
-    await LastCommands.findOneAndDelete({
-      userTelegramId,
-    })
-  return await sendMessage({
-    chat_id: userTelegramId,
-    // text: JSON.stringify({ body, headers: req.headers.origin }),
-    text,
-    keyboard,
-  })
-}
-
-var keyboardCreateTeamSetName = inlineKeyboard([
-  [
-    {
-      text: 'Отмена создания/редактирования команды',
-      callback_data: '/create_team/exit',
-    },
-  ],
-])
-
-var keyboardEditTeamSetName = inlineKeyboard([
-  [
-    {
-      text: 'Отмена редактирования команды',
-      callback_data: '/edit_team/exit',
-    },
-  ],
-])
-
-var keyboardCreateTeamSetDescription = inlineKeyboard([
-  [
-    {
-      text: 'Без описания',
-      callback_data: '/create_team/no_description',
-    },
-  ],
-  [
-    {
-      text: 'Отмена создания/редактирования команды',
-      callback_data: '/create_team/exit',
-    },
-  ],
-])
-
-var keyboardEditTeamSetDescription = inlineKeyboard([
-  [
-    {
-      text: 'Без описания',
-      callback_data: '/edit_team/no_description',
-    },
-  ],
-  [
-    {
-      text: 'Отмена редактирования команды',
-      callback_data: '/edit_team/exit',
-    },
-  ],
-])
-
-var keyboardMainMenu = inlineKeyboard([
-  [
-    {
-      text: 'Команды',
-      callback_data: '/menu_teams',
-    },
-  ],
-  [
-    {
-      text: 'Моя анкета',
-      callback_data: '/menu_user',
-    },
-  ],
-])
-
-var keyboardTeamsMenu = inlineKeyboard([
-  [
-    {
-      text: 'Создать новую команду',
-      callback_data: '/create_team',
-    },
-  ],
-  [
-    {
-      text: 'Редактировать команду',
-      callback_data: '/edit_team',
-    },
-  ],
-  [
-    {
-      text: 'Присоединиться к команде',
-      callback_data: '/join_team',
-    },
-  ],
-  [
-    {
-      text: '<= Вернуться в Главное меню',
-      callback_data: '/main_menu',
-    },
-  ],
-])
-
-var keyboardUserMenu = inlineKeyboard([
-  [
-    {
-      text: '<= Вернуться в Главное меню',
-      callback_data: '/main_menu',
-    },
-  ],
-])
-
-const mainMenuScript = async (userTelegramId) =>
-  await script({
-    userTelegramId,
-    text: `Главное меню`,
-    keyboard: keyboardMainMenu,
-  })
-
-const teamsMenuScript = async (userTelegramId) =>
-  await script({
-    userTelegramId,
-    text: `Меню работы с командами`,
-    keyboard: keyboardTeamsMenu,
-  })
-
-const userMenuScript = async (userTelegramId) =>
-  await script({
-    userTelegramId,
-    text: `Моя анкета`,
-    keyboard: keyboardUserMenu,
-  })
-
-const getLastCommand = async (userTelegramId) => {
-  await dbConnect()
-  const lastCommand = await LastCommands.find({ userTelegramId })
-  const isLastCommandExists = lastCommand && lastCommand.length !== 0
-  if (!isLastCommandExists) return
-
-  const lastCommandsArray = lastCommand[0].command.get('command').split('/')
-  lastCommandsArray.shift()
-  const mainCommand = lastCommandsArray[0]
-  const secondaryCommand = lastCommandsArray[1]
-  const props = lastCommand[0].command.get('props')
-  return { mainCommand, secondaryCommand, props }
-}
-
-const allCommands = [
-  'start', // +
-  'menu_teams',
-  'menu_user',
-  'create_team',
-  'edit_team',
-  'join_team',
-  'main_menu', // +
-]
-
 const menus = async (userId, props) => {
   await dbConnect()
   const teamsOfUser = await Teams.find({ capitanId: userId })
@@ -361,6 +181,33 @@ const lastCommandHandler = async (telegramId, command, props, message) => {
 //   return keyboard
 // }
 
+const executeCommand = async (userTelegramId, message) => {
+  await dbConnect()
+  const last = await LastCommands.findOneAndUpdate(
+    {
+      userTelegramId,
+    },
+    {
+      command: {
+        command: message,
+        // props: { teamName: message },
+      },
+    },
+    { upsert: true }
+  )
+  const { command, props } = messageToCommandAndProps(message)
+
+  const result = await lastCommandHandler(userTelegramId, command, props)
+  const keyboard = keyboardFormer(commandsArray, result.buttons)
+
+  return await sendMessage({
+    chat_id: userTelegramId,
+    // text: JSON.stringify({ body, headers: req.headers.origin }),
+    text: result.message,
+    keyboard,
+  })
+}
+
 const commandHandler = async (userTelegramId, message, res) => {
   try {
     // if (message === '/start') {
@@ -385,53 +232,7 @@ const commandHandler = async (userTelegramId, message, res) => {
     const isItCommand = message[0] === '/'
     // Если была отправлена команда, то ищем ее или возвращаем ошибку
     if (isItCommand) {
-      const last = await LastCommands.findOneAndUpdate(
-        {
-          userTelegramId,
-        },
-        {
-          command: {
-            command: message,
-            // props: { teamName: message },
-          },
-        },
-        { upsert: true }
-      )
-      const { command, props } = messageToCommandAndProps(message)
-
-      const result = await lastCommandHandler(userTelegramId, command, props)
-      // await sendMessage({
-      //   chat_id: userTelegramId,
-      //   // text: JSON.stringify({ body, headers: req.headers.origin }),
-      //   text: result.message,
-      //   // keyboard,
-      // })
-
-      // const menu = await menus(userTelegramId, props)
-
-      // if (!menu[command]) {
-      //   const lastCommand = last ? last.command.get('command') : undefined
-      //   return await script({
-      //     userTelegramId,
-      //     command: lastCommand,
-      //     text: 'Неизвестная команда',
-      //   })
-      // }
-
-      // const { text, buttons } = menu[command]
-      const keyboard = keyboardFormer(commandsArray, result.buttons)
-
-      return await sendMessage({
-        chat_id: userTelegramId,
-        // text: JSON.stringify({ body, headers: req.headers.origin }),
-        text: result.message,
-        keyboard,
-      })
-      // return await script({
-      //   userTelegramId,
-      //   text,
-      //   keyboard,
-      // })
+      return executeCommand(userTelegramId, message)
     } else {
       // Если было отправлено сообщение, то смотрим какая до этого была команда (на что ответ)
       const last = await LastCommands.findOne({
@@ -462,58 +263,60 @@ const commandHandler = async (userTelegramId, message, res) => {
       })
 
       if (result.nextCommand) {
-        const { command, props } = messageToCommandAndProps(result.nextCommand)
-        const menu = await menus(userTelegramId, props)
-        const { text, buttons } = menu[command]
+        return await executeCommand(userTelegramId, result.nextCommand)
+        // const { command, props } = messageToCommandAndProps(result.nextCommand)
+        // const menu = await menus(userTelegramId, props)
+        // const { text, buttons } = menu[command]
 
-        await LastCommands.findOneAndUpdate(
-          {
-            userTelegramId,
-          },
-          {
-            command: {
-              command: result.nextCommand,
-              // props: { teamName: message },
-            },
-          },
-          { upsert: true }
-        )
+        // await LastCommands.findOneAndUpdate(
+        //   {
+        //     userTelegramId,
+        //   },
+        //   {
+        //     command: {
+        //       command: result.nextCommand,
+        //       // props: { teamName: message },
+        //     },
+        //   },
+        //   { upsert: true }
+        // )
 
-        const keyboard = keyboardFormer(menu, buttons)
+        // const keyboard = keyboardFormer(menu, buttons)
 
-        return await sendMessage({
-          chat_id: userTelegramId,
-          // text: JSON.stringify({ body, headers: req.headers.origin }),
-          text,
-          keyboard,
-        })
+        // return await sendMessage({
+        //   chat_id: userTelegramId,
+        //   // text: JSON.stringify({ body, headers: req.headers.origin }),
+        //   text,
+        //   keyboard,
+        // })
       }
+      return await executeCommand(userTelegramId, '/' + result.upper_command)
 
-      const menu = await menus(userTelegramId, {})
-      const { upper_command } = menu[command]
-      const { text, buttons } = menu[upper_command]
+      // const menu = await menus(userTelegramId, {})
+      // const { upper_command } = menu[command]
+      // const { text, buttons } = menu[upper_command]
 
-      await LastCommands.findOneAndUpdate(
-        {
-          userTelegramId,
-        },
-        {
-          command: {
-            command: '/' + upper_command,
-            // props: { teamName: message },
-          },
-        },
-        { upsert: true }
-      )
+      // await LastCommands.findOneAndUpdate(
+      //   {
+      //     userTelegramId,
+      //   },
+      //   {
+      //     command: {
+      //       command: '/' + upper_command,
+      //       // props: { teamName: message },
+      //     },
+      //   },
+      //   { upsert: true }
+      // )
 
-      const keyboard = keyboardFormer(menu, buttons)
+      // const keyboard = keyboardFormer(menu, buttons)
 
-      return await sendMessage({
-        chat_id: userTelegramId,
-        // text: JSON.stringify({ body, headers: req.headers.origin }),
-        text,
-        keyboard,
-      })
+      // return await sendMessage({
+      //   chat_id: userTelegramId,
+      //   // text: JSON.stringify({ body, headers: req.headers.origin }),
+      //   text,
+      //   keyboard,
+      // })
     }
   } catch (e) {
     console.log('e :>> ', e)
