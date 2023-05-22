0883afe6f3f6534b370a978af0bd62f28cd69381
Escalion <escalion86@gmail.com>
2023-05-22 16:05:45 +0700

    test9 joined_teams

--- schemas/teamsUsersSchema.js
+++ schemas/teamsUsersSchema.js
@@ -4,7 +4,7 @@ const teamsUsersSchema = {
     required: [true, 'Необходимо выбрать команду'],
   },
   userTelegramId: {
-    type: String,
+    type: Number,
     required: [true, 'Необходимо указать телеграм id пользователя'],
   },
   role: {
