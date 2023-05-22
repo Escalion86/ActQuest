f6e5d8474769a85e064d0b6ce4dd11b9a3bac61c
Escalion <escalion86@gmail.com>
2023-05-22 15:31:22 +0700

    test9 joined_teams

--- telegram/commands/link_to_join_team.js
+++ telegram/commands/link_to_join_team.js
@@ -1,45 +1,18 @@
-import Teams from '@models/Teams'
-import TeamsUsers from '@models/TeamsUsers'
-import Users from '@models/Users'
-import dbConnect from '@utils/dbConnect'
+import getTeam from 'telegram/func/getTeam'
 
 const link_to_join_team = async ({ telegramId, message, props }) => {
   if (!props?.teamId) {
-    await dbConnect()
-    const teamsUsers = await TeamsUsers.find({ teamId: props?.teamId })
-    if (!teamsUsers || teamsUsers.length === 0) {
+    const team = await getTeam(props?.teamId)
+    if (!team || team.length === 0) {
       return {
-        message: 'Никто не состоит в команде',
+        message: 'Ошибка. Команда не найдена',
         nextCommand: `/menu_teams`,
       }
     }
-    const usersTelegramIds = teamsUsers.map(
-      (teamUser) =>
-        // mongoose.Types.ObjectId(teamUser.teamId)
-        teamUser.userTelegramId
-    )
-
-    const users = await Users.find({
-      telegramId: { $in: usersTelegramIds },
-    })
 
     return {
-      message: 'Состав команды',
-      buttons: [
-        ...users.map((user) => {
-          const teamUser = teamsUsers.find((teamUser) => {
-            return teamUser.userTelegramId === user.telegramId
-          })
-          // const role = teamUser.role === 'capitan' ? 'Капитан' : 'Участник'
-          return {
-            text: `${user.name}${
-              teamUser.role === 'capitan' ? ' (Капитан)' : ''
-            }`,
-            command: `user/userId=${user._id}`,
-          }
-        }),
-        { command: 'menu_teams', text: '\u{2B05} Назад' },
-      ],
+      message: `Код для присоединения к команде: <code>${props?.teamId}</code>.\n Отправьте его пользователю, которого хотите пригласить. Этот кот необходимо ввести в поле "Присоединиться к команде"`,
+      buttons: [{ command: 'menu_teams', text: '\u{2B05} Назад' }],
     }
   }
 }
