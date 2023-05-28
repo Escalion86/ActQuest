f31cbe336d5d162b13b948caad287cd0618aa4bb
Escalion <escalion86@gmail.com>
2023-05-28 22:30:38 +0700

    test13 fixed createGame

--- telegram/func/createGameFunc.js
+++ telegram/func/createGameFunc.js
@@ -2,6 +2,7 @@ import Games from '@models/Games'
 import dbConnect from '@utils/dbConnect'
 
 const createGameFunc = async (userTelegramId, props) => {
+  console.log('createGameFunc props :>> ', props)
   await dbConnect()
   const game = await Games.create(props)
   return game
