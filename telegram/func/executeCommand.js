881a18f19c5ef463249663aac0a7adf758f8f5d7
Escalion <escalion86@gmail.com>
2023-05-31 00:29:41 +0700

    Fix editTask

--- telegram/func/executeCommand.js
+++ telegram/func/executeCommand.js
@@ -6,6 +6,11 @@ import sendMessage from 'telegram/sendMessage'
 import keyboardFormer from './keyboardFormer'
 
 const lastCommandHandler = async (telegramId, jsonCommand) => {
+  console.log('jsonCommand.cmd :>> ', jsonCommand.cmd)
+  console.log(
+    'commandsArray[jsonCommand.cmd] :>> ',
+    !!commandsArray[jsonCommand.cmd]
+  )
   if (commandsArray[jsonCommand.cmd])
     return await commandsArray[jsonCommand.cmd]({ telegramId, jsonCommand })
   return {
