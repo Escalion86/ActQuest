ce365c7b3a46a8f7e7a8d9be390be488ff999d34
Escalion <escalion86@gmail.com>
2023-05-31 23:01:37 +0700

    Added teams + fixes

--- telegram/commands/teams.js
+++ telegram/commands/teams.js
@@ -1,19 +1,27 @@
 import Teams from '@models/Teams'
 import TeamsUsers from '@models/TeamsUsers'
 import dbConnect from '@utils/dbConnect'
-import { MAX_TEAMS } from 'telegram/constants'
 
 const teams = async ({ telegramId, jsonCommand }) => {
   await dbConnect()
   const teams = await Teams.find({})
+  const teamsUsers = await TeamsUsers.find({})
 
   return {
     message: '<b>Обзор всех команд</b>',
     buttons: [
       ...teams.map((team) => {
+        const participansCount = teamsUsers.reduce(
+          (p, c) => p + (c.teamId === String(team._id) ? 1 : 0),
+          0
+        )
         return {
-          text: `"${team.name}"`,
-          cmd: { cmd: 'teamUsers', teamId: team._id, backСmd: 'teams' },
+          text: `"${team.name}" (${participansCount} чел)`,
+          cmd: {
+            cmd: 'teamUsers',
+            teamId: team._id,
+            // , backСmd: 'teams'
+          },
         }
       }),
       {
