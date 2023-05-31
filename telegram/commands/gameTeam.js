4b319a5b464a5ecb309aff09349d67f36b08844b
Escalion <escalion86@gmail.com>
2023-05-31 21:49:57 +0700

    Update gameTeam

--- telegram/commands/gameTeam.js
+++ telegram/commands/gameTeam.js
@@ -1,3 +1,4 @@
+import Teams from '@models/Teams'
 import TeamsUsers from '@models/TeamsUsers'
 import check from 'telegram/func/check'
 import getGame from 'telegram/func/getGame'
@@ -17,13 +18,27 @@ const gameTeam = async ({ telegramId, jsonCommand }) => {
   const team = await getTeam(gameTeam.teamId)
   if (team.success === false) return team
 
-  const teamUser = await TeamsUsers.findOne({
-    userTelegramId: telegramId,
+  const teamUsers = await TeamsUsers.findOne({
+    // userTelegramId: telegramId,
     teamId: String(team._id),
   })
 
+  const usersTelegramIds = teamUsers.map((teamUser) => teamUser.userTelegramId)
+
+  const users = await Teams.find({
+    telegramId: { $in: usersTelegramIds },
+  })
+
+  const teamUser = teamUsers.find(
+    (teamUser) => teamUser.userTelegramId === telegramId
+  )
+
   return {
-    message: `<b>Игра "${game.name}"\nКоманда "${team?.name}"</b>`,
+    message: `<b>Игра "${game.name}"\n\nКоманда "${
+      team?.name
+    }"</b>\n\n<b>Состав команды</b>: ${users
+      .map((user) => ` - ${user.name}`)
+      .join('\n')}`,
     buttons: [
       {
         cmd: {
