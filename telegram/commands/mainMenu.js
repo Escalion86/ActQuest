d4d8c5694fa08d02bbcd1fb828eb22fc7a756dab
Escalion <escalion86@gmail.com>
2023-06-01 20:25:44 +0700

    teams func added

--- telegram/commands/mainMenu.js
+++ telegram/commands/mainMenu.js
@@ -9,6 +9,11 @@ const mainMenu = async ({ telegramId, jsonCommand }) => {
       { c: 'menuTeams', text: '\u{1F465} Команды' },
       { c: 'menuUser', text: '\u{1F464} Моя анкета' },
       {
+        c: 'users',
+        text: '\u{1F6E0} Пользователи без команд',
+        hide: telegramId !== ADMIN_TELEGRAM_ID,
+      },
+      {
         c: 'teams',
         text: '\u{1F6E0} Обзор всех команд',
         hide: telegramId !== ADMIN_TELEGRAM_ID,
