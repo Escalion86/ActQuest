faed587b6cc0fba0399f377a88c23ba381f9fe85
Escalion <escalion86@gmail.com>
2023-06-02 19:26:11 +0700

    test

---
+++ telegram/commands/gameStart.js
@@ -0,0 +1,46 @@
+import Games from '@models/Games'
+import GamesTeams from '@models/GamesTeams'
+import Teams from '@models/Teams'
+import TeamsUsers from '@models/TeamsUsers'
+import check from 'telegram/func/check'
+import formatGameName from 'telegram/func/formatGameName'
+import getGame from 'telegram/func/getGame'
+
+const game = async ({ telegramId, jsonCommand }) => {
+  const checkData = check(jsonCommand, ['gameId'])
+  if (checkData) return checkData
+
+  const game = await getGame(jsonCommand.gameId)
+  if (game.success === false) return game
+
+  await Games.findByIdAndUpdate(jsonCommand.gameId, {
+    status: 'started',
+  })
+  // Получаем список команд
+  const gameTeams = await GamesTeams.find({
+    gameId: jsonCommand.gameId,
+  })
+
+  const teamsIds = gameTeams.map((gameTeam) => gameTeam.teamId)
+
+  const teams = await Teams.find({
+    _id: { $in: teamsIds },
+  })
+
+  const teamsUsers = await TeamsUsers.find({
+    teamId: { $in: teamsIds },
+  })
+
+  const usersIds = teamsUsers.map((teamUser) => teamUser.userId)
+
+  console.log('usersIds :>> ', usersIds)
+
+  return {
+    message: `Игра ${formatGameName(
+      game
+    )} ЗАПУЩЕНА.\n\n\u{26A0} Все игроки оповещены!`,
+    nextCommand: { c: 'editGame', gameId: jsonCommand.gameId },
+  }
+}
+
+export default game
