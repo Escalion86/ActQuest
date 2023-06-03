3fec69f56bc02b9413695bb9a910e253eee2f030
Escalion <escalion86@gmail.com>
2023-06-03 17:01:59 +0700

    sendMessage fix

--- telegram/commands/gameActive.js
+++ telegram/commands/gameActive.js
@@ -16,7 +16,7 @@ const gameStop = async ({ telegramId, jsonCommand }) => {
   if (game.success === false) return game
 
   await Games.findByIdAndUpdate(jsonCommand.gameId, {
-    status: 'finished',
+    status: 'active',
   })
   // Получаем список команд участвующих в игре
   const gameTeams = await GamesTeams.find({
