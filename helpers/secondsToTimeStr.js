4fc3653a5e1afc05ade15076e53e30b32c66776b
Escalion <escalion86@gmail.com>
2023-08-18 23:09:29 +0700

    Rebuild editGame

--- helpers/secondsToTimeStr.js
+++ helpers/secondsToTimeStr.js
@@ -5,9 +5,9 @@ const secondsToTimeStr = (seconds) => {
   const h = Math.floor(seconds / 3600)
   const m = Math.floor((seconds % 3600) / 60)
   const s = seconds % 60
-  return `${h > 0 ? getNoun(h, 'час', 'часа', 'часов') : ''}
-  ${m > 0 ? getNoun(m, 'минута', 'минуты', 'минут') : ''}
-  ${s > 0 ? getNoun(s, 'секунда', 'секунды', 'секунд') : ''}`
+  return `${h > 0 ? getNoun(h, 'час', 'часа', 'часов') : ''}${
+    m > 0 ? getNoun(m, 'минута', 'минуты', 'минут') : ''
+  }${s > 0 ? getNoun(s, 'секунда', 'секунды', 'секунд') : ''}`
 }
 
 export default secondsToTimeStr
