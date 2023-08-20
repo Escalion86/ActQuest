69d384e8235458c826124fed99946ab7c0fcc8b0
Escalion <escalion86@gmail.com>
2023-08-21 00:55:01 +0700

    test

--- telegram/commands/gameAnonsMsg.js
+++ telegram/commands/gameAnonsMsg.js
@@ -35,8 +35,8 @@ const gameAnonsMsg = async ({ telegramId, jsonCommand }) => {
   const users = await Users.find({})
 
   // Получаем telegramId всех пользователей
-  // const allUsersTelegramIds = [261102161]
-  const allUsersTelegramIds = users.map((user) => user.telegramId)
+  const allUsersTelegramIds = [261102161, 1984922390, 5960834042]
+  // const allUsersTelegramIds = users.map((user) => user.telegramId)
 
   await Promise.all(
     allUsersTelegramIds.map(async (telegramId) => {
