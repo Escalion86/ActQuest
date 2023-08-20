b9cbd106bab75a616c6732673b835054da4fad71
Escalion <escalion86@gmail.com>
2023-08-21 00:05:11 +0700

    Update keyboardFormer

--- telegram/func/keyboardFormer.js
+++ telegram/func/keyboardFormer.js
@@ -1,6 +1,35 @@
 import { commandToNum } from 'telegram/commands/commandsArray'
 import inlineKeyboard from './inlineKeyboard'
 
+const buttonConstructor = ({ text, c, url }) => {
+  if (typeof c === 'string')
+    return [
+      {
+        text,
+        callback_data: c ? JSON.stringify({ c: commandToNum[c] }) : undefined,
+        url,
+      },
+    ]
+
+  // Значит команда в JSON формате
+  if (c) {
+    const convertedCommand = { ...c, c: commandToNum[c.c] }
+    return [
+      {
+        text,
+        callback_data: JSON.stringify(convertedCommand),
+        url,
+      },
+    ]
+  }
+  return [
+    {
+      text,
+      url,
+    },
+  ]
+}
+
 const keyboardFormer = (buttons) => {
   if (buttons && typeof buttons === 'object') {
     const resultKeyboard = inlineKeyboard(
@@ -11,56 +40,9 @@ const keyboardFormer = (buttons) => {
           // if (typeof button === 'object')
           if (Array.isArray(button)) {
             const buttonsArray = button.filter((button) => !button.hide)
-            return buttonsArray.map(({ text, c, url }) => {
-              if (typeof c === 'string')
-                return {
-                  text,
-                  callback_data: c
-                    ? JSON.stringify({ c: commandToNum[c] })
-                    : undefined,
-                  url,
-                }
-              else {
-                const convertedCommand = { ...c, c: commandToNum[c.c] }
-                return {
-                  text,
-                  callback_data: c
-                    ? JSON.stringify(convertedCommand)
-                    : undefined,
-                  url,
-                }
-              }
-            })
+            return buttonsArray.map(buttonConstructor)
           }
-          const { text, c, url } = button
-          if (typeof c === 'string')
-            return [
-              {
-                text,
-                callback_data: c
-                  ? JSON.stringify({ c: commandToNum[c] })
-                  : undefined,
-                url,
-              },
-            ]
-
-          // Значит команда в JSON формате
-          if (c) {
-            const convertedCommand = { ...c, c: commandToNum[c.c] }
-            return [
-              {
-                text,
-                callback_data: JSON.stringify(convertedCommand),
-                url,
-              },
-            ]
-          }
-          return [
-            {
-              text,
-              url,
-            },
-          ]
+          return buttonConstructor(button)
         })
     )
     return resultKeyboard
