530959f142954ff24d168bdec60fbe6f3762755e
Escalion <escalion86@gmail.com>
2023-08-21 00:22:43 +0700

    Update keyboardFormer

--- telegram/func/keyboardFormer.js
+++ telegram/func/keyboardFormer.js
@@ -3,50 +3,40 @@ import inlineKeyboard from './inlineKeyboard'
 
 const buttonConstructor = ({ text, c, url }) => {
   if (typeof c === 'string')
-    return [
-      {
-        text,
-        callback_data: c ? JSON.stringify({ c: commandToNum[c] }) : undefined,
-        url,
-      },
-    ]
+    return {
+      text,
+      callback_data: c ? JSON.stringify({ c: commandToNum[c] }) : undefined,
+      url,
+    }
 
   // Значит команда в JSON формате
   if (c) {
     const convertedCommand = { ...c, c: commandToNum[c.c] }
-    return [
-      {
-        text,
-        callback_data: JSON.stringify(convertedCommand),
-        url,
-      },
-    ]
-  }
-  return [
-    {
+    return {
       text,
+      callback_data: JSON.stringify(convertedCommand),
       url,
-    },
-  ]
+    }
+  }
+  return {
+    text,
+    url,
+  }
 }
 
 const keyboardFormer = (buttons) => {
   if (buttons && typeof buttons === 'object') {
-    const resultKeyboard = inlineKeyboard(
-      // await Promise.all(
+    return inlineKeyboard(
       buttons
         .filter((button) => !button.hide)
         .map((button) => {
-          // if (typeof button === 'object')
           if (Array.isArray(button)) {
             const buttonsArray = button.filter((button) => !button.hide)
             return buttonsArray.map(buttonConstructor)
           }
-          return buttonConstructor(button)
+          return [buttonConstructor(button)]
         })
     )
-    return resultKeyboard
-    // )
   }
   return
 }
