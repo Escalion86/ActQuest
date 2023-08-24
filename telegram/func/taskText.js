4b801a0fa97a275ebdbfad167c7c0b74017f577e
Escalion <escalion86@gmail.com>
2023-08-25 00:58:16 +0700

    Fix taskText

--- telegram/func/taskText.js
+++ telegram/func/taskText.js
@@ -15,7 +15,7 @@ const taskText = ({
   const showCluesNum = Math.floor(taskDuration / cluesDuration)
 
   const cluesText = ''
-  if (showCluesNum > 0)
+  if (cluesDuration > 0 && showCluesNum > 0)
     for (let i = 0; i < showCluesNum; i++) {
       if (clues[i]?.clue)
         cluesText += `\n\n<b>Подсказка №${i + 1}</b>:\n${clues[i].clue}`
@@ -24,7 +24,9 @@ const taskText = ({
   return `<b>Задание №${
     taskNum + 1
   }:</b>\n${task}${cluesText}${`\n\n<b>Время до ${
-    taskDuration < cluesDuration * 2 ? 'подсказки' : 'завершения задания'
+    cluesDuration > 0 && showCluesNum < clues?.length
+      ? 'подсказки'
+      : 'завершения задания'
   }</b>: ${secondsToTime(
     cluesDuration - (taskDuration % cluesDuration)
   )}`}\n\nКоличество кодов на локации: ${codes?.length ?? 0}${
