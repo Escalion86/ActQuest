74c84daf462824fbb4ed83a53285529d42deb3b0
Escalion <escalion86@gmail.com>
2023-08-31 11:38:51 +0700

    Update teamUsersAdmin

--- telegram/commands/teamUsersAdmin.js
+++ telegram/commands/teamUsersAdmin.js
@@ -32,13 +32,14 @@ const teamUsersAdmin = async ({ telegramId, jsonCommand }) => {
   })
 
   const usersWithRoleInTeam = users.map((user) => {
+    console.log('user :>> ', user)
     const teamUser = teamsUsers.find((teamUser) => {
       return teamUser.userTelegramId === user.telegramId
     })
     return { ...user, role: teamUser?.role, teamUserId: teamUser._id }
   })
   usersWithRoleInTeam.sort((user) => (user?.role === 'capitan' ? -1 : 1))
-  console.log('usersWithRoleInTeam :>> ', usersWithRoleInTeam)
+  // console.log('usersWithRoleInTeam :>> ', usersWithRoleInTeam)
 
   const page = jsonCommand?.page ?? 1
   const buttons = buttonListConstructor(
