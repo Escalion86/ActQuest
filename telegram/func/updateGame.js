d02dd8ded9a52826e6f4491b233ae767946f1c18
Escalion <escalion86@gmail.com>
2023-09-28 01:58:37 +0700

    Add penalty code

---
+++ telegram/func/updateGame.js
@@ -0,0 +1,25 @@
+import Games from '@models/Games'
+import dbConnect from '@utils/dbConnect'
+import mongoose from 'mongoose'
+
+const updateGame = async (id, props) => {
+  await dbConnect()
+  if (id === undefined || !mongoose.Types.ObjectId.isValid(id))
+    return {
+      success: false,
+      message: 'Ошибка. gameId не указан',
+      nextCommand: `mainMenu`,
+    }
+
+  const game = await Games.findByIdAndUpdate(id, props)
+  if (!game) {
+    return {
+      success: false,
+      message: 'Ошибка. Нет такого gameId',
+      nextCommand: `mainMenu`,
+    }
+  }
+  return game
+}
+
+export default updateGame
