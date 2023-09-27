650675d50555cb08e7ecd2a094dc30dd57ee5f74
Escalion <escalion86@gmail.com>
2023-09-28 01:54:51 +0700

    Add penalty code

--- telegram/commands/editPenaltyCode.js
+++ telegram/commands/editPenaltyCode.js
@@ -1,5 +1,6 @@
 import secondsToTimeStr from '@helpers/secondsToTimeStr'
 import Games from '@models/Games'
+import dbConnect from '@utils/dbConnect'
 import check from 'telegram/func/check'
 import getGame from 'telegram/func/getGame'
 
