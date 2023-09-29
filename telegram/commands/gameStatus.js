c7fb7e43ae229412bdfbfc432934061d4dfa5fd1
Escalion <escalion86@gmail.com>
2023-09-30 01:45:41 +0700

    Fixes

--- telegram/commands/gameStatus.js
+++ telegram/commands/gameStatus.js
@@ -32,69 +32,77 @@ const gameStatus = async ({ telegramId, jsonCommand }) => {
 
   console.log('teams :>> ', teams)
 
-  const text = teams
-    .map((team) => {
-      const gameTeam = gameTeams.find(
-        (gameTeam) => gameTeam.teamId === String(team._id)
-      )
-      var startedTasks = 0
-      gameTeam.startTime.forEach((time) => {
-        if (time) ++startedTasks
-      })
-      const findedCodes =
-        gameTeam.findedCodes?.length >= startedTasks
-          ? gameTeam.findedCodes[startedTasks - 1].length
-          : 0
-      const findedBonusCodes =
-        gameTeam.findedBonusCodes?.length >= startedTasks
-          ? gameTeam.findedBonusCodes[startedTasks - 1].length
-          : 0
-      const findedPenaltyCodes =
-        gameTeam.findedPenaltyCodes?.length >= startedTasks
-          ? gameTeam.findedPenaltyCodes[startedTasks - 1].length
-          : 0
-      const taskDuration = game.taskDuration ?? 3600
-
-      const isTeamFinished =
-        gameTeam.endTime[game.tasks.length - 1] ||
-        (gameTeam.startTime[game.tasks.length - 1] &&
-          getSecondsBetween(gameTeam.startTime[game.tasks.length - 1]) >
-            taskDuration)
-
-      if (isTeamFinished) return `"${team.name}" - завершили все задания`
-
-      // Проверяем, может задание выполнено и команда на перерыве
-      if (gameTeam.endTime[startedTasks - 1]) {
-        const nextTask = game.tasks[startedTasks]
-        return `"${team.name}" - на перерыве след задание №${
-          startedTasks + 1
-        } "${nextTask.title}"`
-      }
-
-      const task = game.tasks[startedTasks - 1]
-      return `"${team.name}" - выполняют задание №${startedTasks} "${
-        task.title
-      }".${
-        findedCodes > 0
-          ? `\nНайденые коды (${findedCodes} шт.): "${gameTeam.findedCodes[
-              startedTasks - 1
-            ].join(`", "`)}"`
-          : ''
-      }${
-        findedCodes > 0
-          ? `\nНайденые бонусные коды (${findedBonusCodes} шт.): "${gameTeam.findedBonusCodes[
-              startedTasks - 1
-            ].join(`", "`)}"`
-          : ''
-      }${
-        findedCodes > 0
-          ? `\nНайденые штрафные коды (${findedPenaltyCodes} шт.): "${gameTeam.findedPenaltyCodes[
-              startedTasks - 1
-            ].join(`", "`)}"`
-          : ''
-      }`
+  const textArray = teams.map((team) => {
+    const gameTeam = gameTeams.find(
+      (gameTeam) => gameTeam.teamId === String(team._id)
+    )
+    console.log('gameTeam :>> ', gameTeam)
+    var startedTasks = 0
+    gameTeam.startTime.forEach((time) => {
+      if (time) ++startedTasks
     })
-    .join('\n\n')
+    const findedCodes =
+      gameTeam.findedCodes?.length >= startedTasks
+        ? gameTeam.findedCodes[startedTasks - 1].length
+        : 0
+    const findedBonusCodes =
+      gameTeam.findedBonusCodes?.length >= startedTasks
+        ? gameTeam.findedBonusCodes[startedTasks - 1].length
+        : 0
+    const findedPenaltyCodes =
+      gameTeam.findedPenaltyCodes?.length >= startedTasks
+        ? gameTeam.findedPenaltyCodes[startedTasks - 1].length
+        : 0
+    const taskDuration = game.taskDuration ?? 3600
+
+    const isTeamFinished =
+      gameTeam.endTime[game.tasks.length - 1] ||
+      (gameTeam.startTime[game.tasks.length - 1] &&
+        getSecondsBetween(gameTeam.startTime[game.tasks.length - 1]) >
+          taskDuration)
+
+    console.log('isTeamFinished :>> ', isTeamFinished)
+    if (isTeamFinished) return `"${team.name}" - завершили все задания`
+
+    // Проверяем, может задание выполнено и команда на перерыве
+    console.log(
+      'gameTeam.endTime[startedTasks - 1] :>> ',
+      gameTeam.endTime[startedTasks - 1]
+    )
+    if (gameTeam.endTime[startedTasks - 1]) {
+      const nextTask = game.tasks[startedTasks]
+      return `"${team.name}" - на перерыве след задание №${startedTasks + 1} "${
+        nextTask.title
+      }"`
+    }
+
+    const task = game.tasks[startedTasks - 1]
+    console.log('!')
+    return `"${team.name}" - выполняют задание №${startedTasks} "${
+      task.title
+    }".${
+      findedCodes > 0
+        ? `\nНайденые коды (${findedCodes} шт.): "${gameTeam.findedCodes[
+            startedTasks - 1
+          ].join(`", "`)}"`
+        : ''
+    }${
+      findedCodes > 0
+        ? `\nНайденые бонусные коды (${findedBonusCodes} шт.): "${gameTeam.findedBonusCodes[
+            startedTasks - 1
+          ].join(`", "`)}"`
+        : ''
+    }${
+      findedCodes > 0
+        ? `\nНайденые штрафные коды (${findedPenaltyCodes} шт.): "${gameTeam.findedPenaltyCodes[
+            startedTasks - 1
+          ].join(`", "`)}"`
+        : ''
+    }`
+  })
+  console.log('textArray :>> ', textArray)
+
+  const text = textArray.join('\n\n')
 
   // const tasksDuration = gameTeams.map((gameTeam) => ({
   //   teamId: gameTeam.teamId,
