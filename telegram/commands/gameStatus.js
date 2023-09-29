86c4ee7cc23df83977c8df8fc9681e4dd419932a
Escalion <escalion86@gmail.com>
2023-09-30 02:02:13 +0700

    Fixes

--- telegram/commands/gameStatus.js
+++ telegram/commands/gameStatus.js
@@ -10,27 +10,27 @@ const numberToEmojis = (number) => {
   const emojis = digits.map((digit) => {
     switch (digit) {
       case '0':
-        return `\u{0030}`
+        return `\u{0030}\u{20E3}`
       case '1':
-        return `\u{0031}`
+        return `\u{0031}\u{20E3}`
       case '2':
-        return `\u{0032}`
+        return `\u{0032}\u{20E3}`
       case '3':
-        return `\u{0033}`
+        return `\u{0033}\u{20E3}`
       case '4':
-        return `\u{0034}`
+        return `\u{0034}\u{20E3}`
       case '5':
-        return `\u{0035}`
+        return `\u{0035}\u{20E3}`
       case '6':
-        return `\u{0036}`
+        return `\u{0036}\u{20E3}`
       case '7':
-        return `\u{0037}`
+        return `\u{0037}\u{20E3}`
       case '8':
-        return `\u{0038}`
+        return `\u{0038}\u{20E3}`
       case '9':
-        return `\u{0039}`
+        return `\u{0039}\u{20E3}`
       default:
-        return `\u{002A}`
+        return `\u{002A}\u{20E3}`
     }
   })
   return emojis.join('')
