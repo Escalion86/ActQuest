b764dcaa14c8ae4dc0327a267b290c63e43ba660
Escalion <escalion86@gmail.com>
2023-09-30 02:38:19 +0700

    Fixes

--- telegram/commands/gameStatus.js
+++ telegram/commands/gameStatus.js
@@ -4,37 +4,7 @@ import GamesTeams from '@models/GamesTeams'
 import Teams from '@models/Teams'
 import check from 'telegram/func/check'
 import getGame from 'telegram/func/getGame'
-
-const numberToEmojis = (number) => {
-  var digits = number.toString().split('')
-  const emojis = digits.map((digit) => {
-    switch (digit) {
-      case '0':
-        return `\u{0030}\u{20E3}`
-      case '1':
-        return `\u{0031}\u{20E3}`
-      case '2':
-        return `\u{0032}\u{20E3}`
-      case '3':
-        return `\u{0033}\u{20E3}`
-      case '4':
-        return `\u{0034}\u{20E3}`
-      case '5':
-        return `\u{0035}\u{20E3}`
-      case '6':
-        return `\u{0036}\u{20E3}`
-      case '7':
-        return `\u{0037}\u{20E3}`
-      case '8':
-        return `\u{0038}\u{20E3}`
-      case '9':
-        return `\u{0039}\u{20E3}`
-      default:
-        return `\u{002A}\u{20E3}`
-    }
-  })
-  return emojis.join('')
-}
+import numberToEmojis from 'telegram/func/numberToEmojis'
 
 const gameStatus = async ({ telegramId, jsonCommand }) => {
   const checkData = check(jsonCommand, ['gameId'])
---
+++ telegram/func/numberToEmojis.js
@@ -0,0 +1,32 @@
+const numberToEmojis = (number) => {
+  var digits = number.toString().split('')
+  const emojis = digits.map((digit) => {
+    switch (digit) {
+      case '0':
+        return `\u{0030}\u{20E3}`
+      case '1':
+        return `\u{0031}\u{20E3}`
+      case '2':
+        return `\u{0032}\u{20E3}`
+      case '3':
+        return `\u{0033}\u{20E3}`
+      case '4':
+        return `\u{0034}\u{20E3}`
+      case '5':
+        return `\u{0035}\u{20E3}`
+      case '6':
+        return `\u{0036}\u{20E3}`
+      case '7':
+        return `\u{0037}\u{20E3}`
+      case '8':
+        return `\u{0038}\u{20E3}`
+      case '9':
+        return `\u{0039}\u{20E3}`
+      default:
+        return `\u{002A}\u{20E3}`
+    }
+  })
+  return emojis.join('')
+}
+
+export default numberToEmojis
