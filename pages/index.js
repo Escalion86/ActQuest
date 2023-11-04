84c230b848f987541109abc72acdbcb8c3974290
Escalion <escalion86@gmail.com>
2023-11-04 23:27:10 +0700

    tests

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import Script from 'next/script'
+import TelegramLoginButton from 'react-telegram-login'
+
+const handleTelegramResponse = (response) => {
+  console.log(response)
+}
 
 export default function Home(props) {
   return (
@@ -7,6 +13,23 @@ export default function Home(props) {
         <title>{`ActQuest`}</title>
       </Head>
       <div>{'ActQuest'}</div>
+      <TelegramLoginButton
+        dataOnauth={handleTelegramResponse}
+        botName="ActQuest_dev_bot"
+      />
+      {/* <Script
+        async
+        src="https://telegram.org/js/telegram-widget.js?22"
+        data-telegram-login="ActQuest_dev_bot"
+        data-size="large"
+        data-onauth="onTelegramAuth(user)"
+        data-request-access="write"
+      ></Script>
+      <Script type="text/javascript">
+        {`function onTelegramAuth(user) {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }`}
+      </Script> */}
     </>
   )
 }
