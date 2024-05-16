b8d7f598ed6861f4d20333adfb2a6e07260c608c
Escalion <escalion86@gmail.com>
2024-05-17 03:01:14 +0700

    Update

--- telegram/commands/editGame.js
+++ telegram/commands/editGame.js
@@ -81,8 +81,8 @@ const editGame = async ({ telegramId, jsonCommand }) => {
             gameId: jsonCommand.gameId,
           },
           text: game.hideResult
-            ? '\u{1F648} Скрыть результаты'
-            : '\u{1F648} Открыть результаты',
+            ? '\u{1F648} Открыть результаты'
+            : '\u{1F648} Скрыть результаты',
           hide: !game.result,
         },
       ],
