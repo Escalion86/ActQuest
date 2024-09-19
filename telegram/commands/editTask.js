244bbfe80c6f408b6133212acb8c3eb9da22bc61
Escalion <escalion86@gmail.com>
2024-09-19 19:58:44 +0700

    SubTasks creating process

--- telegram/commands/editTask.js
+++ telegram/commands/editTask.js
@@ -47,7 +47,7 @@ const editTask = async ({ telegramId, jsonCommand }) => {
     message: `<b>Редактирование задания</b>\n"${
       task?.title
     }"\n\n<b>Текст задания</b>:${
-      task?.task ? '{не задано]' : `\n<blockquote>${task?.task}</blockquote>`
+      !task?.task ? '{не задано]' : `\n<blockquote>${task?.task}</blockquote>`
     }${cluesText}${
       game.type === 'photo'
         ? ''
