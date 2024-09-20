e7ff749b51ca995184818608c937a354a9f69697
Escalion <escalion86@gmail.com>
2024-09-21 04:03:00 +0700

    SubTasks creating process

--- telegram/commands/gameTeamCheckPhotosInTask.js
+++ telegram/commands/gameTeamCheckPhotosInTask.js
@@ -119,8 +119,7 @@ const gameTeamCheckPhotosInTask = async ({ telegramId, jsonCommand, user }) => {
     : 0
 
   return {
-    images:
-      typeof jsonCommand?.taskAcceptChange === 'boolean' ? undefined : photos,
+    images: jsonCommand?.taskAcceptChange ? undefined : photos,
     message: `Проверка фотографий в игре <b>${formatGameName(
       game
     )}</b> у команды "<b>${
