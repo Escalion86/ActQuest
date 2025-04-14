fc24f35c8051d395a72adcc2086b67cc6cd4e9e3
Escalion <escalion86@gmail.com>
2025-04-14 13:09:28 +0700

    testoff

--- telegram/commandHandler.js
+++ telegram/commandHandler.js
@@ -73,6 +73,20 @@ const commandHandler = async ({
     //   })
     // }
 
+    if (message === '/testoff') {
+      return await sendMessage({
+        chat_id: userTelegramId,
+        // text: JSON.stringify({ body, headers: req.headers.origin }),
+        text: 'Test',
+        location,
+        keyboard: {
+          keyboard: [],
+          // resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      })
+    }
+
     var jsonCommand
     if (message && message[0] === '/') {
       jsonCommand = { c: message.substr(1) }
