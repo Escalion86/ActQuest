ea86ff5e49251f0c68145fd1f84797d58aaabb2f
Escalion <escalion86@gmail.com>
2025-08-25 03:00:10 +0700

    Fix USERS_ROLES

--- telegram/constants.js
+++ telegram/constants.js
@@ -1,9 +1,10 @@
 export const MAX_TEAMS = 3
 export const DEV_TELEGRAM_ID = 261102161
 
-export const USERS_ROLES = ['client', 'admin', 'dev']
+export const USERS_ROLES = ['client', 'admin', 'dev', 'ban']
 export const USERS_ROLES_NAMES = {
   client: 'Пользователь',
   admin: 'Администратор',
   dev: 'Разработчик',
+  ban: 'Бан',
 }
