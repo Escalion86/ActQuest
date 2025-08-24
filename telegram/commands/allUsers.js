c1817803e8f2865bf814c8f1c9d2d034bef31fbe
Escalion <escalion86@gmail.com>
2025-08-25 03:00:22 +0700

    Update allUsers (added filter)

--- telegram/commands/allUsers.js
+++ telegram/commands/allUsers.js
@@ -1,10 +1,11 @@
+import userRole from '@helpers/userRole'
 import buttonListConstructor from 'telegram/func/buttonsListConstructor'
 
 const allUsers = async ({ telegramId, jsonCommand, location, db }) => {
-  const users = await db.model('Users').find({})
-  const teamsUsers = await db.model('TeamsUsers').find({})
+  const users = await db.model('Users').find({}).lean()
+  const teamsUsers = await db.model('TeamsUsers').find({}).lean()
+  const games = await db.model('Games').find({}).lean()
 
-  const games = await db.model('Games').find({})
   var allTeamsUsersInFinishedGames = []
   games.forEach(({ result }) => {
     if (result) {
@@ -15,8 +16,21 @@ const allUsers = async ({ telegramId, jsonCommand, location, db }) => {
     }
   })
 
+  const filteredUsers = users.filter((user) => {
+    switch (userRole(user)) {
+      case 'client':
+        return jsonCommand.showClients
+      case 'admin':
+        return jsonCommand.showAdmin
+      case 'ban':
+        return jsonCommand.showBan
+      default:
+        return false
+    }
+  })
+
   const page = jsonCommand?.page ?? 1
-  const buttons = buttonListConstructor(users, page, (user, number) => {
+  const buttons = buttonListConstructor(filteredUsers, page, (user, number) => {
     const teamsOfUserCount = teamsUsers.filter(
       (teamsUser) => teamsUser.userTelegramId === user.telegramId
     ).length
@@ -35,8 +49,31 @@ const allUsers = async ({ telegramId, jsonCommand, location, db }) => {
   })
 
   return {
-    message: `<b>Обзор всех пользователей (${users.length} чел.)\nВ скобках указано количество команд / сыграных игр</b>`,
+    message: `<b>Все пользователи согласно фильтру (${filteredUsers.length} чел.)\nВ скобках указано количество команд / сыграных игр</b>`,
     buttons: [
+      [
+        {
+          c: {
+            showClients: !jsonCommand.showClients,
+            page: 1,
+          },
+          text: (jsonCommand.showClients ? '✅' : '❌') + ' Пользователи',
+        },
+        {
+          c: {
+            showAdmin: !jsonCommand.showAdmin,
+            page: 1,
+          },
+          text: (jsonCommand.showAdmin ? '✅' : '❌') + ' Админы',
+        },
+        {
+          c: {
+            showBan: !jsonCommand.showBan,
+            page: 1,
+          },
+          text: (jsonCommand.showBan ? '✅' : '❌') + ' Бан',
+        },
+      ],
       ...buttons,
       {
         c: 'adminMenu',
